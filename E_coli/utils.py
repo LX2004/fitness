7bef0bbf222929b70694f1737cddd766c43c616c
LX2004 <77057762+LX2004@users.noreply.github.com>
2024-12-04 13:02:36 +0800

    Update utils.py

--- E_coli/utils.py
+++ E_coli/utils.py
@@ -68,10 +68,10 @@ def encode_coding(coding):
 
 
 def write_good_record(dict1,dict2,file_path):
-    # 合并两个字典
+
     merged_dict = {**dict1, **dict2}
 
-    # 检查文件是否存在，如果不存在，则创建
+
     if not os.path.isfile(file_path):
         with open(file_path, 'w') as file:
             file.write(f"{merged_dict}\n")
@@ -81,10 +81,9 @@ def write_good_record(dict1,dict2,file_path):
 
 def one_hot(sequence):
     bases = ['A','T','G','C']
-    # 创建一个空的数组用于储存one-hot编码结果
+
     one_hot_encoded = np.zeros((len(sequence), len(bases)))
 
-    # 填充one-hot编码数组
     for i, base in enumerate(sequence):
         one_hot_encoded[i, bases.index(base)] = 1
         
@@ -123,70 +122,56 @@ def text_build_vocab():
     return dic
 
 def transformer_index_to_ATCGseq(data):
-    #data = torch.randn(4, 100)  # 假设这是你的张量
-    # 将索引映射到 "A", "T", "C", "G"
     max_indices = torch.argmax(data, dim=0)
     max_indices = max_indices.to('cpu').numpy()
-    # print('max_indices =',max_indices)
+
     mapping = {0: "A", 1: "T", 2: "C", 3: "G"}
     sequence = [mapping[i] for i in max_indices]
-# 将结果连接成一个字符串
+
     sequence_str = ''.join(sequence)
     return sequence_str
     
 def trans_output_to_input(fake_im):
-    # 将由噪音生成的数据进行编码，得到输入评价器的数据。首先转化为原始序列，再转化为np数组
+
     sample_seq = []
     for num_sample in range(fake_im.shape[0]):
         sample_one = fake_im[num_sample,0,:,:]
         sample_seq.append(transformer_index_to_ATCGseq(sample_one))
-    # print('sample_seq = ',sample_seq) 
-    # pdb.set_trace()
+
     sample_result = []
     for seq in sample_seq:
         sample_result.append(Dimer_split_seqs(seq))
         
     sample_result = np.array(sample_result)
     sample_result = np.expand_dims(sample_result, axis=1)
+    
     tensor = torch.from_numpy(sample_result)
     fake_img = tensor.to('cuda')
     return fake_img
 
 
 def Dimer_split_seqs(seq):
+    
     t = text_build_vocab()
-    # print('t = ', t)
-    # pdb.set_trace()
     ori_result = []
     dim_result = []
     pos_result = []
     
     result = ''
-
     lens = len(seq)
 
     for i in range(lens):
         result += ' ' + seq[i].upper()
         ori_result.append(t.index(seq[i].upper()))
 
-    # dimer_encode
-    # result += ' '
-    # result += 'SEP1'
-
     seq += '0'
     wt = 2
     for i in range(lens):
         result += ' ' + seq[i:i + wt].upper()
         dim_result.append(t.index(seq[i:i + wt].upper()))
-    
-    # print('result = ',result)
-    
-    # pdb.set_trace()
 
     pos_result += [i for i in range(1, lens + 1)]
-    # print('ori_result = ', ori_result)
-    # print('dim_result = ', dim_result)
-    # print('pos_result = ', pos_result)
+
     if ori_result[0] < 0:
         pdb.set_trace()
         print('seq = ', seq)
@@ -222,44 +207,25 @@ def compute_correlation_coefficient(output,label):
     has_nan = np.isnan(prediction).any() or np.isnan(target).any()
 
     if has_nan:
-        print("数组中存在NaN值")
+        print("There are NaN values ​​in the array")
 
     if np.std(prediction) == 0:
-        print('预测数据没有波动')
+        print('No fluctuations in forecast data')
         return 0
     
     if np.std(target) == 0:
-        print('真实数据没有波动')
+        print('The real data does not fluctuate')
         return 0
-
-
-    # 计算平均值
 
     mean_target = np.mean(target)
     mean_prediction = np.mean(prediction)
 
-    # 计算协方差
     covariance = np.mean((target - mean_target) * (prediction - mean_prediction))
 
-    # 计算标准差
     std_target = np.std(target)
     std_prediction = np.std(prediction)
 
-    # 计算皮尔逊相关系数
     pearson_coefficient = covariance / (std_target * std_prediction)
 
 
-    # print('target.shape = ',target.shape)
-    # print('prediction.shape = ',prediction.shape)
-
-
-    # # 使用NumPy的corrcoef函数计算皮尔逊相关系数
-    # correlation_matrix = np.corrcoef(target,prediction)
-    
-    # pearson_coefficient = correlation_matrix[0, 1]
-
     return pearson_coefficient
-
-# oris = ['-','+','0']
-# for ori in oris:
-#     encode_ori(ori)
